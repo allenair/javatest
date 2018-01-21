705000f06cf919e7120a876a3b22770b0a4163c7
Allen Wang <allenaiir@gmail.com>
2018-01-21 10:11:37 +0800

    chinese number change

--- src/main/java/com/zxtech/numchange/ChineseToArabNum.java
+++ src/main/java/com/zxtech/numchange/ChineseToArabNum.java
@@ -6,8 +6,11 @@ import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 
 public class ChineseToArabNum {
+	private static String[] ALL_KEYWORD = new String[] { "亿", "万", "千", "百", "十", "零", "0", "九", "9", "八", "8", "七",
+			"7", "六", "6", "五", "5", "四", "4", "三", "3", "二", "2", "一", "1" };
 	private static String[] POST_NAME = new String[] { "亿", "万", "千", "百", "十" };
 	private static Map<String, Integer> NUMBER_Map = new HashMap<>();
+	private static Map<String, String> CHN_NUMBER_Map = new HashMap<>();
 	private static Map<String, String> STR_MAP = new HashMap<>();
 	private static Map<String, Integer> POST_MAP = new HashMap<>();
 	static {
@@ -35,15 +38,73 @@ public class ChineseToArabNum {
 		NUMBER_Map.put("三", 3);
 		NUMBER_Map.put("二", 2);
 		NUMBER_Map.put("一", 1);
+		NUMBER_Map.put("0", 0);
+		NUMBER_Map.put("9", 9);
+		NUMBER_Map.put("8", 8);
+		NUMBER_Map.put("7", 7);
+		NUMBER_Map.put("6", 6);
+		NUMBER_Map.put("5", 5);
+		NUMBER_Map.put("4", 4);
+		NUMBER_Map.put("3", 3);
+		NUMBER_Map.put("2", 2);
+		NUMBER_Map.put("1", 1);
+
+		CHN_NUMBER_Map.put("0", "零");
+		CHN_NUMBER_Map.put("9", "九");
+		CHN_NUMBER_Map.put("8", "八");
+		CHN_NUMBER_Map.put("7", "七");
+		CHN_NUMBER_Map.put("6", "六");
+		CHN_NUMBER_Map.put("5", "五");
+		CHN_NUMBER_Map.put("4", "四");
+		CHN_NUMBER_Map.put("3", "三");
+		CHN_NUMBER_Map.put("2", "二");
+		CHN_NUMBER_Map.put("1", "一");
 	}
 
 	public static void main(String[] args) {
-		System.out.println(cal("一百九十五万三千五百二十三"));
+		// System.out.println(cal("一百九十五万三千五百二十三"));
+		System.out.println(getNumStr("gjhjk一百95万三千五百二十三dadas"));
 	}
 
 	public static long cal(String str) {
 		String cstr = normalizeStr(str);
 		return chinese2ArabNum(cstr, 0, 0);
+	}
+
+	public static long getNumStr(String srcStr) {
+		StringBuilder sb = new StringBuilder();
+		String tmp;
+		for (int i = 0; i < srcStr.length(); i++) {
+			tmp = "" + srcStr.charAt(i);
+			if (isInArray(tmp, ALL_KEYWORD)) {
+				sb.append(tmp);
+			} else {
+				if (sb.toString().length() > 0) {
+					break;
+				}
+			}
+		}
+		tmp = sb.toString();
+		for (String post : POST_NAME) {
+			if (tmp.contains(post)) {
+				break;
+			}
+		}
+
+		return cal(tmp);
+	}
+
+	private static boolean isInArray(String src, String[] all_keyword) {
+		for (int i = 0; i < src.length(); i++) {
+			String tmp = "" + src.charAt(i);
+			for (String keyWord : all_keyword) {
+				if (keyWord.equals(tmp)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
 	}
 
 	private static String normalizeStr(String str) {
@@ -71,8 +132,16 @@ public class ChineseToArabNum {
 			return NUMBER_Map.get(cstr) * (long) (Math.pow(10, postNum));
 		}
 
-		if (cstr.length() == 2 && +cstr.charAt(0) == '零') {
+		if (cstr.length() == 2 && cstr.charAt(0) == '零') {
 			return NUMBER_Map.get(cstr.substring(1)) * (long) (Math.pow(10, postNum));
+		}
+
+		if (!isInArray(cstr, POST_NAME)) {
+			try {
+				return Long.parseLong(cstr)* (long) (Math.pow(10, postNum));
+			} catch (Exception e) {
+				return 0;
+			}
 		}
 
 		for (String post : POST_NAME) {
