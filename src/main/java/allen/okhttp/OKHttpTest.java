ddddbd8395e9a211b1ff7b74a76a3e32c7d5a986
syp560 <syp560@PC-syp560.neuxa.com>
2018-03-14 16:46:05 +0800

    cc

--- src/main/java/allen/okhttp/OKHttpTest.java
+++ src/main/java/allen/okhttp/OKHttpTest.java
@@ -1,6 +1,7 @@
 package allen.okhttp;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,9 +43,26 @@ public class OKHttpTest {
 //			}).start();
 //		}
 		
-		String url = "http://localhost:8080/ess/api/savecallfixdataforiot.io?hardCode=80063&errorDescript=aaaaallllleeeeennnnn" + "&peopleFlag=1";
-		new OKHttpTest().getFunAsyn(url);
-
+//		String url = "http://localhost:8080/ess/api/savecallfixdataforiot.io?hardCode=80063&errorDescript=aaaaallllleeeeennnnn" + "&peopleFlag=1";
+//		new new OKHttpTest()().getFunAsyn(url);
+		
+		final OKHttpTest okhttp = new OKHttpTest();
+		final String url = "http://172.16.8.78:8080/essiot/ftiotdata.io";
+		final String json="{\"elevatorId\":\"allentest123\",\"parameterStr\":\"ExX+D31ubgAAY3oBAA==\",\"time\":\"12345678\"}";
+		okhttp.post(url, json);
+		
+		IntStream.range(1, 220).forEach(n->{
+			Thread tt = new Thread(()->{
+				try {
+					okhttp.post(url, json);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			
+			tt.start();
+		});
+		
 	}
 	
 	public void getFunAsyn(String url) throws Exception {
