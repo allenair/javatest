f61b3cf1a64c9d2424c15c19b78a9dd707e09f00
allenair <allenair@gmail.com>
2019-12-30 11:29:22 +0800

    1230

---
+++ src/main/java/allen/nlp/JianFanTest.java
@@ -0,0 +1,17 @@
+package allen.nlp;
+
+import java.util.UUID;
+
+import org.nlpcn.commons.lang.jianfan.JianFan;
+
+public class JianFanTest {
+
+	public static void main(String[] args) {
+		System.out.println(JianFan.f2j("清對濁，苦對鹹，一啓對三緘。煙蓑對雨笠，月榜對風帆。鶯睍睆，燕呢喃，柳杞對鬆杉。情深悲素扇，淚痛溼青衫。漢室既能分四姓，周朝何用叛三監。破的而探牛心，豪矜王濟；豎竿以掛犢鼻，貧笑阮咸"));
+	
+		System.out.println(UUID.randomUUID());
+		
+	
+	}
+
+}
