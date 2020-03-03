bb8ebae2748a1ba28d1b3091f237d184d68650c2
allenair <allenair@126.com>
2020-03-03 17:19:02 +0800

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -44,7 +44,7 @@ public class Test {
 
 	private void tt190614() {
 		int c=11;
-		int sdd=12;
+		int sddww=12;
 		List<Integer> sortedList = new ArrayList<>();
 		sortedList.add(11000);
 		sortedList.add(9001);
