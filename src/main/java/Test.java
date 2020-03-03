7015132620d6738129972589e67b2eef918d32a8
allenair <allenair@126.com>
2020-03-03 17:18:29 +0800

    ss

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -37,14 +37,14 @@ public class Test {
 	 */
 	public static void main(String[] args) throws Exception {
 		String ss="ss";
-		char c;
+		char gg;
 		Test tt = new Test();
 		tt.tt190614();
 	}
 
 	private void tt190614() {
 		int c=11;
-		int s=12;
+		int sdd=12;
 		List<Integer> sortedList = new ArrayList<>();
 		sortedList.add(11000);
 		sortedList.add(9001);
